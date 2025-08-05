e1ba7ac0d0df1f855548a4a9e9321a1afc6546fe
YOMI-YJ <pcbg301@naver.com>
2025-08-05 15:29:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,7 @@ def get_four_pillars(birth_str: str):
 
 # ===== Streamlit 앱 시작 =====
 st.set_page_config(page_title="🔮 사주팔자 해석 챗봇", layout="centered")
-st.markdown("<h1>🔮 재미로 보는 GPT 기반 사주 해석</h1>", unsafe_allow_html=True)
-st.markdown("<small>(feat. 4천년의 통계학!)</small>", unsafe_allow_html=True)
+st.markdown("<h3>🔮 4천년의 통계학! 재미로 보는 GPT 기반 사주 해석</h3>", unsafe_allow_html=True)
 
 
 
